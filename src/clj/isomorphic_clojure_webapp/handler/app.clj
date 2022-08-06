b22b6915a2f7642681da56be11c78a6663913697
宮本七々子 <naaaaaaa.tooo@gmail.com>
2022-08-06 10:08:10 +0900

    :sparkles:取得APIの実装

--- src/clj/isomorphic_clojure_webapp/handler/app.clj
+++ src/clj/isomorphic_clojure_webapp/handler/app.clj
@@ -1,7 +1,10 @@
 (ns isomorphic-clojure-webapp.handler.app
   (:require [integrant.core :as ig]
-            [rum.core :refer [render-html]]))
+            [rum.core :refer [render-html]]
+            [next.jdbc :as jdbc]
+            [honeysql.format :as sql]))
 
+(def db-spec (fn [db] (:datasource (:spec db))))
 
 (defmethod ig/init-key ::index [_ _]
   (fn [req]
@@ -13,12 +16,22 @@
                           [:#app]
                           [:script {:src "/js/main.js"}]]])}))
 
-(defmethod ig/init-key ::add [_ _]
+(defmethod ig/init-key ::all [_ {:keys [db]}]
   (fn [req]
-    (prn req)
+    (let [tasks (jdbc/execute! (db-spec db) (sql/format {:select [:*] :from [:tasks]}))]
+      {:status 200
+       :headers {"content-type" "application/json"}
+       :body tasks})))
+
+(defmethod ig/init-key ::fetch [_ {:keys [db]}]
+  (fn [req]
+    (let [tasks (jdbc/execute! (db-spec db) [(format "select * from tasks where id = %s" (get-in req [:path-params :id]))])] 
+      {:status 200
+       :headers {"content-type" "application/json"}
+       :body (first tasks)})))
+
+(defmethod ig/init-key ::add [_ {:keys [db]}]
+  (fn [req]
     {:status 200
-     :headers {"content-type" "text/html"}
-     :body (render-html [:html
-                         [:body
-                          [:#app]
-                          [:script {:src "/js/main.js"}]]])}))
+       :headers {"content-type" "application/json"}
+       :body "ok"}))
