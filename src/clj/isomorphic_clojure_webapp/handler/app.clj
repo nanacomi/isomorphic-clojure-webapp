49873302a0a5215a23a42f0e141abad4e5861ffa
宮本七々子 <naaaaaaa.tooo@gmail.com>
2022-08-06 11:24:45 +0900

    :sparkles:追加・修正・削除APIを実装

--- src/clj/isomorphic_clojure_webapp/handler/app.clj
+++ src/clj/isomorphic_clojure_webapp/handler/app.clj
@@ -32,6 +32,31 @@
 
 (defmethod ig/init-key ::add [_ {:keys [db]}]
   (fn [req]
-    {:status 200
+    (let [new-label (get (clojure.walk/keywordize-keys (get req :body-params)) :label)]
+      ;; (jdbc/execute! (db-spec db) [(format "insert into tasks(\"label\") values(\"%s\")" new-label)])
+      (jdbc/execute! (db-spec db) (sql/format {:insert-into :tasks,
+                                               :columns [:label],
+                                               :values [[new-label]]}))
+      {:status 200
        :headers {"content-type" "application/json"}
-       :body "ok"}))
+       :body "ok"})))
+
+(defmethod ig/init-key ::update [_ {:keys [db]}]
+  (fn [req]
+    (let [id (get-in req [:path-params :id])
+          label (get (clojure.walk/keywordize-keys (get req :body-params)) :label)]
+      (jdbc/execute! (db-spec db) (sql/format {:update :tasks,
+                                               :set {:label label},
+                                               :where [:= :id id]}))
+      {:status 200
+       :headers {"content-type" "application/json"}
+       :body "ok"})))
+
+(defmethod ig/init-key ::delete [_ {:keys [db]}]
+  (fn [req]
+    (let [id (get-in req [:path-params :id])]
+      (jdbc/execute! (db-spec db) (sql/format {:delete-from :tasks
+                                               :where [:= :id id]}))
+      {:status 200
+       :headers {"content-type" "application/json"}
+       :body "ok"})))
