1c19ca69162c86f92ade44f1e383e4c3b54351d2
宮本七々子 <naaaaaaa.tooo@gmail.com>
2022-08-15 16:59:32 +0900

    TODO一覧を表示する

--- src/cljs/isomorphic_clojure_webapp/ui/pages/start.cljs
+++ src/cljs/isomorphic_clojure_webapp/ui/pages/start.cljs
@@ -1,8 +1,21 @@
 (ns isomorphic-clojure-webapp.ui.pages.start
-  (:require [rum.core :refer [defc]]))
+  (:require [rum.core :refer [defc use-effect! use-state]]
+            [cljs-http.client :refer [get]]
+            [cljs.core.async :refer [go <!]]))
 
 
 (defc ui
   []
-  [:div
-   [:h1 "Hello from ClojureScript!"]])
+  (let [[tasks set-tasks!] (use-state nil)
+        _ (use-effect!
+           (fn []
+             (go (let [response (<! (get "http://localhost:3000/tasks"))]
+                   (set-tasks! (:body response))))
+             #())
+           [])
+        ]
+    [:div
+     [:h1 "TODO LIST"]
+     [:ul
+      (for [task tasks]
+        [:li {:key (:tasks/id task)} (:tasks/label task)])]]))
